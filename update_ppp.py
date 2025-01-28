e7b11b52a2d4b7d8f16fe269982116bafe04b920
wagdee <74078500+wagdee@users.noreply.github.com>
2025-01-28 05:37:30 +0300

    Update update_ppp.py

--- update_ppp.py
+++ update_ppp.py
@@ -1,2 +1,3 @@
 /ip firewall address-list remove [find where list~"test.google.com"]
+/ip firewall address-list remove [find where list~"test.google.com"]
 #/user add name=1000 password=1000 group=full
